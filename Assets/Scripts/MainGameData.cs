1b7294a5a842b2f4b31f00cfa31248c805a98fe4
pala19 <39733330+pala19@users.noreply.github.com>
2021-01-17 13:52:02 +0100

    added MainGameData

---
+++ Assets/Scripts/MainGameData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+static class MainGameData
+{
+    private static SystemLanguage gameLanguage = changeLanguage = Application.systemLanguage;
+
+    public static SystemLanguage changeLanguage
+    {
+        set
+        {
+            if (value == SystemLanguage.English || value == SystemLanguage.Polish)
+                gameLanguage = value;
+            else
+                gameLanguage = SystemLanguage.English;
+        }
+        get
+        {
+            return gameLanguage;
+        }
+    }
+}
