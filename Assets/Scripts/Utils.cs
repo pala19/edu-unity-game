29d7cd50e7ac7b2de76a1ba965a0048df76ab5df
pala19 <39733330+pala19@users.noreply.github.com>
2021-03-28 15:15:28 +0200

    tests setup

--- Assets/Scripts/Utils.cs
+++ Assets/Scripts/Utils.cs
@@ -11,9 +11,13 @@ public class Utils
     {
         int[] ResultPermutation = new int[n];
         ResultPermutation = Enumerable.Range(0, n+1).ToArray();
-        System.Random random = new System.Random(System.Guid.NewGuid().GetHashCode());
+        System.Random random = GetNewRandom();
 
         return ResultPermutation.OrderBy(x => random.Next()).ToArray();
+    }
+    public static System.Random GetNewRandom()
+    {
+        return new System.Random(System.Guid.NewGuid().GetHashCode());
     }
 
 
---
+++ Assets/Tests/EditTests/UtilsTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+
+namespace Tests
+{
+    public class UtilsTest
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void UtilsTestRandomAreNeverEqual()
+        {
+            var random1 = Utils.GetNewRandom();
+            var random2 = Utils.GetNewRandom();
+            Assert.AreNotEqual(random1, random2);
+        }
+        [Test]
+        public void UtilsTestPermutationIsRandom()
+        {
+            var perm1 = Utils.GenerateRandomPermutation(5);
+            var perm2 = Utils.GenerateRandomPermutation(5);
+            Assert.AreNotEqual(perm1, perm2);
+        }
+    }
+}
