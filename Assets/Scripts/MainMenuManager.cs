40918dcbd697f041e64612f6990bb438100c8a61
pala19 <39733330+pala19@users.noreply.github.com>
2021-04-11 13:27:09 +0200

    add new menu buttons and improve exisiting ones

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -38,17 +38,23 @@ public class MainMenuManager : MonoBehaviour
         for (int i=0; i<CountGameBtns.Length; i++)
         {
             if (CountGameData.IsActive(i))
-                CountGameBtns[i].transform.GetChild(1).gameObject.SetActive(false);
+                CountGameBtns[i].transform.GetChild(0).gameObject.SetActive(false);
+            else
+                CountGameBtns[i].GetComponent<SVGImage>().material = Disabled;
         }
         for (int i = 0; i <AddGameBtns.Length; i++)
         {
             if (AddGameData.IsActive(i))
-                AddGameBtns[i].transform.GetChild(1).gameObject.SetActive(false);
+                AddGameBtns[i].transform.GetChild(0).gameObject.SetActive(false);
+            else
+                AddGameBtns[i].GetComponent<SVGImage>().material = Disabled;
         }
         for (int i = 0; i < SubGameBtns.Length; i++)
         {
             if (SubGameData.IsActive(i))
-                SubGameBtns[i].transform.GetChild(1).gameObject.SetActive(false);
+                SubGameBtns[i].transform.GetChild(0).gameObject.SetActive(false);
+            else
+                SubGameBtns[i].GetComponent<SVGImage>().material = Disabled;
         }
         MainGameData.PressedButton = true;
     }
@@ -65,17 +71,17 @@ public class MainMenuManager : MonoBehaviour
     }
     public void SecondGameOnClick()
     {
-        if (EventSystem.current.currentSelectedGameObject.transform.GetChild(1).gameObject.activeSelf)
+        if (EventSystem.current.currentSelectedGameObject.transform.GetChild(0).gameObject.activeSelf)
             Scroller2.SetActive(!Scroller2.activeSelf);
     }
     public void ThirdGameOnClick()
     {
-        if (EventSystem.current.currentSelectedGameObject.transform.GetChild(1).gameObject.activeSelf)
+        if (EventSystem.current.currentSelectedGameObject.transform.GetChild(0).gameObject.activeSelf)
             Scroller3.SetActive(!Scroller3.activeSelf);
     }
     public void PlayCountGameOnClick(int Round)
     {
-        if (!EventSystem.current.currentSelectedGameObject.transform.GetChild(1).gameObject.activeSelf)
+        if (!EventSystem.current.currentSelectedGameObject.transform.GetChild(0).gameObject.activeSelf)
         {
             CountGameData.SetCurrentGame = Round;
             SceneManager.LoadScene(1);
@@ -83,7 +89,7 @@ public class MainMenuManager : MonoBehaviour
     }
     public void PlayAddGameOnClick(int Round)
     {
-        if (!EventSystem.current.currentSelectedGameObject.transform.GetChild(1).gameObject.activeSelf)
+        if (!EventSystem.current.currentSelectedGameObject.transform.GetChild(0).gameObject.activeSelf)
         {
             AddGameData.SetCurrentGame = Round;
             SceneManager.LoadScene(2);
@@ -91,7 +97,7 @@ public class MainMenuManager : MonoBehaviour
     }
     public void PlaySubGameOnClick(int Round)
     {
-        if (!EventSystem.current.currentSelectedGameObject.transform.GetChild(1).gameObject.activeSelf)
+        if (!EventSystem.current.currentSelectedGameObject.transform.GetChild(0).gameObject.activeSelf)
         {
             SubGameData.SetCurrentGame = Round;
             SceneManager.LoadScene(3);
