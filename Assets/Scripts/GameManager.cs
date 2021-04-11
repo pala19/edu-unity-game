57ca97e9ddefba7cfaf8325daca37138a726b479
pala19 <39733330+pala19@users.noreply.github.com>
2021-04-11 15:46:02 +0200

    change countables placement

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -55,23 +55,54 @@ public class GameManager : MonoBehaviour
             Vector3 pos = new Vector3(0f, 1f, 90f);
             float height = Screen.currentResolution.height / 4;
             float width;
-            if (CountablesNumber > 5)
-                width = Screen.currentResolution.width / 6;
-            else
-                width = Screen.currentResolution.width / (CountablesNumber + 1);
-            if (i > 4)
+            Countables[i] = Instantiate(CountablePrefab, pos, Quaternion.identity);
+            Countables[i].transform.SetParent(GameObject.Find("Background").transform);
+
+            if (CountablesNumber > 4 && CountablesNumber < 7)
             {
-                width = Screen.currentResolution.width / (CountablesNumber - 4);
-                float pom = i - 5;
-                Countables[i] = Instantiate(CountablePrefab, pos, Quaternion.identity);
-                Countables[i].transform.SetParent(GameObject.Find("Background").transform);
-                Countables[i].transform.localPosition = new Vector3((-Screen.currentResolution.width / 2) + ((pom + 1) * width), -height * 1.6f, 0);
+                width = Screen.currentResolution.width / 4;
+
+                if (i > 2)
+                {
+                    width = Screen.currentResolution.width / (CountablesNumber - 2);
+                    float pom = i - 3;
+                    Countables[i].transform.localPosition = new Vector3((-Screen.currentResolution.width / 2) + ((pom + 1) * width), -height * 1.6f, 0);
+                }
+                else
+                {
+                    Countables[i].transform.localPosition = new Vector3((-Screen.currentResolution.width / 2) + ((i + 1) * width), -height / 1.1f, 0);
+                }
+            }
+            else if (CountablesNumber > 6 && CountablesNumber < 9)
+            {
+                width = Screen.currentResolution.width / 5;
+                if (i > 3)
+                {
+                    width = Screen.currentResolution.width / (CountablesNumber - 3);
+                    float pom = i - 4;
+                    Countables[i].transform.localPosition = new Vector3((-Screen.currentResolution.width / 2) + ((pom + 1) * width), -height * 1.6f, 0);
+                }
+                else
+                {
+                    Countables[i].transform.localPosition = new Vector3((-Screen.currentResolution.width / 2) + ((i + 1) * width), -height / 1.1f, 0);
+                }
             }
             else
             {
-                Countables[i] = Instantiate(CountablePrefab, pos, Quaternion.identity);
-                Countables[i].transform.SetParent(GameObject.Find("Background").transform);
-                Countables[i].transform.localPosition = new Vector3((-Screen.currentResolution.width / 2) + ((i + 1) * width), -height / 1.1f, 0);
+                if (CountablesNumber > 5)
+                    width = Screen.currentResolution.width / 6;
+                else
+                    width = Screen.currentResolution.width / (CountablesNumber + 1);
+                if (i > 4)
+                {
+                    width = Screen.currentResolution.width / (CountablesNumber - 4);
+                    float pom = i - 5;
+                    Countables[i].transform.localPosition = new Vector3((-Screen.currentResolution.width / 2) + ((pom + 1) * width), -height * 1.6f, 0);
+                }
+                else
+                {
+                    Countables[i].transform.localPosition = new Vector3((-Screen.currentResolution.width / 2) + ((i + 1) * width), -height / 1.1f, 0);
+                }
             }
             SetCountableId(i);
         }
